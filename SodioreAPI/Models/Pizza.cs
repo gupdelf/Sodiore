b779db5e83ce33f9b3f9d475f4c37c4e30f35940
Gustavo Pott Delfino <gustavopottpott@gmail.com>
2022-10-22 10:52:19 -0300

    creates pizza model

---
+++ SodioreAPI/Models/Pizza.cs
@@ -0,0 +1,20 @@
+using System.ComponentModel.DataAnnotations;
+
+namespace SodioreAPI.models
+{
+    // defines columns
+    /*  Id
+        name
+        price
+        ?special
+    */
+    public class Pizza
+    {
+        [Key]
+        public int Id {get; set;}
+        
+        public string? Name {get; set;}
+        public float Price {get; set;}
+        public bool isSpecial {get; set;}
+    }
+}
